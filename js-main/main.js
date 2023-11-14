555675d8a8cceabe6ff5314bc830662c39c4670e
liviasz <106825082+liviasz@users.noreply.github.com>
2023-11-14 09:03:41 -0300

    Add files via upload

---
+++ js-main/main.js
@@ -0,0 +1,13 @@
+function playAudio(audioID) {
+    
+    
+    const audio = document.getElementById (audioID);
+    
+    if(audio){
+        audio.play();
+    }else(
+        alert(audioID)
+    )
+    alert(audioID);
+    
+}
